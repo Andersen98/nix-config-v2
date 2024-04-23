fb7a2602c35d8a37a3fc5cfe38c9ef6ae245cffb
Hannah Andersen <thehumanme@gmail.com>
2024-04-22 23:13:44 -0600

    added cuda fhs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,23 @@
 {
   description = "Hannah's NixOS config";
+
+    # the nixConfig here only affects the flake itself, not the system configuration!
+  nixConfig = {
+    # override the default substituters
+    substituters = [
+
+      "https://cache.nixos.org"
+
+      # nix community's cache server
+      "https://cuda-maintainers.cachix.org"
+    ];
+    trusted-public-keys = [
+      #  cuda-maintainers's cache server public key
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
+
   inputs = {
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     home-manager.url = "github:nix-community/home-manager";
@@ -17,6 +35,11 @@
       system = "x86_64-linux";
       "${machine}-${loadout}" = lib.nixosSystem {
         modules = [
+          {
+            # given the users in this list the right to specify additional substituters via:
+            #    1. `nixConfig.substituters` in `flake.nix`
+            nix.settings.trusted-users = [ "root" "hannah" ];
+          }
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
