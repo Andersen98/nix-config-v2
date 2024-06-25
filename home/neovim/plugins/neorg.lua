724f499e1591c4cbcd1bfc94b6e82d570a06592d
Hannah Andersen <thehumanme@gmail.com>
2024-06-25 00:29:10 -0600

    enabled more neorg modules

--- home/neovim/plugins/neorg.lua
+++ home/neovim/plugins/neorg.lua
@@ -6,11 +6,30 @@ require("neorg").setup {
             keybinds.map("norg",
             "n",keybinds.leader .. "lc",
             "<cmd>Neorg keybind all core.looking-glass.magnify-code-block<CR>")
+            keybinds.map("all",
+            "n",keybinds.leader .. "nwd",
+            "<cmd>Neorg workspace default<CR>")
           end,
         }
       },
      ["core.defaults"] = {},
      ["core.concealer"] = {},
+     ["core.summary"] = {},
+     ["core.text-objects"] = {
+        config = {
+          hook = function(keybinds)
+              -- Binds to move items up or down
+              keybinds.remap_event("norg", "n", "<A-k>", "core.text-objects.item_up")
+              keybinds.remap_event("norg", "n", "<A-j>", "core.text-objects.item_down")
+
+              -- text objects, these binds are available as `vaH` to "visual select around a header" or
+              -- `diH` to "delete inside a header"
+              keybinds.remap_event("norg", { "o", "x" }, "iH", "core.text-objects.textobject.heading.inner")
+              keybinds.remap_event("norg", { "o", "x" }, "aH", "core.text-objects.textobject.heading.outer")
+          end,
+
+        },
+     },
      ["core.dirman"] = {
         config = {
           workspaces = {
@@ -18,7 +37,11 @@ require("neorg").setup {
             playground = "~/neorg/playground",
           },
           default_workspace = "playground",
+          open_last_workspace = "default",
         },
-      }
-   }
+      },
+   },
  }
+vim.wo.foldlevel = 99
+vim.wo.conceallevel = 2
+vim.opt.autochdir = true;
