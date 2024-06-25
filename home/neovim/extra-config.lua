0bce8dc0ebf2cdde2e334ea106b72f7198aeb8bb
Hannah <thehumanme@gmail.com>
2024-06-24 18:27:44 -0600

    broken haskell norg

--- home/neovim/extra-config.lua
+++ home/neovim/extra-config.lua
@@ -5,15 +5,15 @@ vim.opt.relativenumber = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
+--vim.opt.formatoptions =  't,c,r,o,/,q,l,1,],j,a'
 vim.opt.textwidth = 52
 vim.opt.wrap = false
-vim.opt.listchars = { space = '_', tab = '>~', trail = '-', nbsp = '+', precedes = '<', extends = '>'}
-vim.opt.sidescroll = 8
+vim.opt.listchars:append({ space = '_', tab = '>~', trail = '-', nbsp = '+', precedes = '<', extends = '>'})
+vim.opt.sidescroll = 5
 
 vim.opt.smarttab = true
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2 vim.opt.tabstop = 2
 vim.cmd([[
   filetype plugin on
   filetype indent on
@@ -30,3 +30,4 @@ vim.opt.termguicolors = true
 vim.g.mapleader = " "
 
 vim.opt.whichwrap = "b,s"
+vim.opt.backspace = "eol"
